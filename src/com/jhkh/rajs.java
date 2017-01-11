865c4ecfff28556dd586fb81e19a976087c6ed8b
devpal <devpal>
2017-01-11 16:03:33 +0530

    this demo GITHUB

---
+++ src/com/jhkh/rajs.java
@@ -0,0 +1,17 @@
+package com.jhkh;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class rajs {
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+	WebDriver driver=new FirefoxDriver();
+	driver.get("http://google.com");
+
+	}
+
+}
